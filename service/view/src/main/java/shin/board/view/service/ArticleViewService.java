2dd52e37f54ccdafdbe86f7f081606824bcb7dcb
kwang <shinkwang@yonsei.ac.kr>
2025-01-16 14:34:44 +0900

    feat: distributed-lock to article-view-count

--- service/view/src/main/java/shin/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/shin/board/view/service/ArticleViewService.java
@@ -2,20 +2,27 @@ package shin.board.view.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import shin.board.view.repository.ArticleViewCountBackUpRepository;
 import shin.board.view.repository.ArticleViewCountRepository;
+import shin.board.view.repository.ArticleViewDistributedLockRepository;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleViewService {
 
     private static final int BACK_UP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10L);
 
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
-
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BATCH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
