263a2b18d29af8bb70a28ac6230e6fdaf1ff9191
kwang <shinkwang@yonsei.ac.kr>
2025-01-18 13:17:14 +0900

    feat: repositories for hot-article

---
+++ service/hot-article/src/main/java/shin/board/hotarticle/repository/ArticleLikeCountRepository.java
@@ -0,0 +1,30 @@
+package shin.board.hotarticle.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Repository
+public class ArticleLikeCountRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    // hot-article::article::{articleId}::like-count
+    private final String KEY_FORMAT = "hot-article::article::{articleId}::like-count";
+
+    public void createOrUpdate(Long articleId, Long likeCount, Duration ttl) {
+        redisTemplate.opsForValue().set(generateKey(articleId), String.valueOf(likeCount), ttl);
+    }
+
+    public Long read(Long articleId) {
+        String result = redisTemplate.opsForValue().get(generateKey(articleId));
+        return result == null ? 0L : Long.valueOf(result);
+    }
+
+    private String generateKey(Long articleId) {
+        return KEY_FORMAT.formatted(articleId);
+    }
+}
