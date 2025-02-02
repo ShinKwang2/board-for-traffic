1e82e93990e8d6f2a432dcd7e49b2f8ca05a4694
kwang <shinkwang@yonsei.ac.kr>
2025-02-02 14:17:57 +0900

    feat: controller for article-read list

--- service/article-read/src/test/java/shin/board/articleread/api/ArticleReadApiTest.java
+++ service/article-read/src/test/java/shin/board/articleread/api/ArticleReadApiTest.java
@@ -2,19 +2,45 @@ package shin.board.articleread.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestClient;
+import shin.board.articleread.service.response.ArticleReadPageResponse;
 import shin.board.articleread.service.response.ArticleReadResponse;
 
 public class ArticleReadApiTest {
 
-    RestClient restClient = RestClient.create("http://localhost:9005");
+    RestClient articleReadRestClient = RestClient.create("http://localhost:9005");
+    RestClient articleRestClient = RestClient.create("http://localhost:9000");
 
     @Test
     void readTest() {
-        ArticleReadResponse response = restClient.get()
+        ArticleReadResponse response = articleReadRestClient.get()
                 .uri("/v1/articles/{articleId}", 145017267107520512L)
                 .retrieve()
                 .body(ArticleReadResponse.class);
 
         System.out.println("response = " + response);
+    }
+
+    @Test
+    void readAllTest() {
+        ArticleReadPageResponse response1 = articleReadRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1L, 5))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response1.getArticleCount() = " + response1.getArticleCount());
+        for (ArticleReadResponse article : response1.getArticles()) {
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+
+        ArticleReadPageResponse response2 = articleRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1L, 5))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response2.getArticleCount() = " + response2.getArticleCount());
+        for (ArticleReadResponse article : response2.getArticles()) {
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+
     }
 }
