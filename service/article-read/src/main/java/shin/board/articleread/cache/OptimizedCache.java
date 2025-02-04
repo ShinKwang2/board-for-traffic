a5a6ad8615c7f642c301d46421d519e3dca7ffe9
kwang <shinkwang@yonsei.ac.kr>
2025-02-05 08:41:31 +0900

    feat:Optimized cacahe for article-read

---
+++ service/article-read/src/main/java/shin/board/articleread/cache/OptimizedCache.java
@@ -0,0 +1,35 @@
+package shin.board.articleread.cache;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.ToString;
+import shin.board.common.dataserializer.DataSerializer;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class OptimizedCache {
+
+    private String data;
+    private LocalDateTime expiredAt;
+
+    // Logical TTL
+    public static OptimizedCache of(Object data, Duration ttl) {
+        OptimizedCache optimizedCache = new OptimizedCache();
+        optimizedCache.data = DataSerializer.serialize(data);
+        optimizedCache.expiredAt = LocalDateTime.now().plus(ttl);
+        return optimizedCache;
+    }
+
+    @JsonIgnore
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiredAt);
+    }
+
+    public <T> T parseData(Class<T> dataType) {
+        return DataSerializer.deserialize(data, dataType);
+    }
+}
